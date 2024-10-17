b75e8bc8da35c46504142a8dba3e621e7239234c
infoset8236 <infoset8236@gmail.com>
2024-10-17 14:24:47 +0900

    [feat] 이용안내 키오스크B가로 시설안내 3840px 완료

--- resources/ict/js/type_b/facility.js
+++ resources/ict/js/type_b/facility.js
@@ -12,28 +12,34 @@ $(document).ready(function () {
 });
 
 $(function () {
-	$('.floor_slide_01').bxSlider({
-		infiniteLoop: false,
-		controls: false,
-		pager: true,
-		slideMargin: 40,
+	const swiper = new Swiper('.floor_slide_01', {
+		slidesPerView: 1, // 한 번에 한 슬라이드만 보이도록 설정
+		spaceBetween: 10, // 슬라이드 사이의 간격 설정 (30px)
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
 	});
 });
 
 $(function () {
-	$('.floor_slide_02').bxSlider({
-		infiniteLoop: false,
-		controls: false,
-		pager: true,
-		slideMargin: 40,
+	const swiper = new Swiper('.floor_slide_02', {
+		slidesPerView: 1, // 한 번에 한 슬라이드만 보이도록 설정
+		spaceBetween: 10, // 슬라이드 사이의 간격 설정 (30px)
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
 	});
 });
 
 $(function () {
-	$('.floor_slide_03').bxSlider({
-		infiniteLoop: false,
-		controls: false,
-		pager: true,
-		slideMargin: 40,
+	const swiper = new Swiper('.floor_slide_03', {
+		slidesPerView: 1, // 한 번에 한 슬라이드만 보이도록 설정
+		spaceBetween: 10, // 슬라이드 사이의 간격 설정 (30px)
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
 	});
 });
