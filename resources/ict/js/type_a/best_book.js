a9472c7ae1148ace42a17158aebdaa02ffe64eaf
infosetGang <kangyoonkyung@infoset.co.kr>
2024-10-17 12:05:05 +0900

    [feat] a타입 대출베스트 작업 완료

---
+++ resources/ict/js/type_a/best_book.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	var swiper = new Swiper('.swiper-container', {
+		loop: true,
+		slidesPerView: 1,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	});
+});
